2ed93772059d08215d9652f28a3fcf5bfa93c0a4
rasheed113 <140941093+rasheed113@users.noreply.github.com>
2026-08-22 06:54:37 +0500

    Make Work_Disk Web routing static-host safe

--- work_disk_web/src/presentation/main.tsx
+++ work_disk_web/src/presentation/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { App } from './app/App'
 import './styles/tokens.css'
 import './styles/global.css'
@@ -13,8 +13,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
